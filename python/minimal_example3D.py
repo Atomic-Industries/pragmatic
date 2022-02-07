d3d9965159281fa4ba3930fd686de23db1c42007
Jared Callaham <jared.callaham@gmail.com>
2022-02-07 15:55:08 -0500

    Updated minimal_example3D

--- python/minimal_example3D.py
+++ python/minimal_example3D.py
@@ -7,13 +7,13 @@
 ### the width of the step, the number of solition<->adaptation iterations as well as the
 ### error level (eta) are optional input parameters
 
-from dolfin import *
+from fenics import *
 from adaptivity import metric_pnorm, logproject, adapt
 from sympy import Symbol, diff
 from sympy import tanh as pytanh
 from sympy import cos as pysin
 from sympy import sin as pycos
-set_log_level(INFO+1)
+from time import time
 #parameters["allow_extrapolation"] = True
 
 def minimal_example3D(width=2e-2, Nadapt=10, eta = 0.04):
@@ -38,27 +38,28 @@ def minimal_example3D(width=2e-2, Nadapt=10, eta = 0.04):
           return x[0]-mesh.coordinates()[:,0].min() < DOLFIN_EPS or mesh.coordinates()[:,0].max()-x[0] < DOLFIN_EPS \
           or mesh.coordinates()[:,1].min() < DOLFIN_EPS or mesh.coordinates()[:,1].max()-x[1] < DOLFIN_EPS \
           or mesh.coordinates()[:,2].min() < DOLFIN_EPS or mesh.coordinates()[:,2].max()-x[2] < DOLFIN_EPS
+    
     # PERFORM TEN ADAPTATION ITERATIONS
     fid  = File("out.pvd")
     for iii in range(Nadapt):
-     V = FunctionSpace(mesh, "CG" ,2); dis = TrialFunction(V); dus = TestFunction(V); u = Function(V)
-     a = inner(grad(dis), grad(dus))*dx
-     L = Expression(ddtestsol)*dus*dx
-     bc = DirichletBC(V, Expression(testsol), boundary)
-     solve(a == L, u, bc)
-     fid << u
-     startTime = time()
-     H = metric_pnorm(u, eta, max_edge_length=2., max_edge_ratio=50, CG1out=True)
-     #H = logproject(H)
-     if iii != Nadapt-1:
-      mesh = adapt(H) 
-      L2error = errornorm(Expression(testsol), u, degree_rise=4, norm_type='L2')
-      log(INFO+1,"total (adapt+metric) time was %0.1fs, L2error=%0.0e, nodes: %0.0f" % (time()-startTime,L2error,mesh.num_vertices()))
-    
+        V = FunctionSpace(mesh, "CG" ,2); dis = TrialFunction(V); dus = TestFunction(V); u = Function(V)
+        a = inner(grad(dis), grad(dus))*dx
+        L = Expression(ddtestsol, degree=2)*dus*dx
+        bc = DirichletBC(V, Expression(testsol, degree=2), boundary)
+        solve(a == L, u, bc)
+        fid << u
+        startTime = time()
+        H = metric_pnorm(u, eta, max_edge_length=2., max_edge_ratio=50, CG1out=True)
+        #H = logproject(H)
+        if iii != Nadapt-1:
+            mesh = adapt(H) 
+            L2error = errornorm(Expression(testsol, degree=2), u, degree_rise=4, norm_type='L2')
+            info("total (adapt+metric) time was %0.1fs, L2error=%0.0e, nodes: %0.0f" % (time()-startTime,L2error,mesh.num_vertices()))
+        
 
     plot(u,interactive=True)
     plot(mesh,interactive=True)
 
 
 if __name__=="__main__":
- minimal_example3D()
+    minimal_example3D()
